63087ab126cfce540c280c44bad64c8d43aa4e46
Megadash452 <megadash452@proton.me>
2026-02-02 19:18:23 -0500

    Fixed Date class not providing correct selectable dates because of mistaken millis interpretation.

--- android/app/src/main/java/com/example/budgiet/Utils.kt
+++ android/app/src/main/java/com/example/budgiet/Utils.kt
@@ -187,26 +187,20 @@ suspend fun <T> runWork(executor: Executor = WORKER_THREAD, task: suspend () -> 
 }
 
 class Date private constructor(private val localDate: LocalDate): ChronoLocalDate {
-    constructor(utcMillis: Long) : this(
-        Instant.ofEpochMilli(utcMillis)
+    constructor(utcTimeMillis: Long) : this(
+        Instant.ofEpochMilli(utcTimeMillis)
             // NOTE: This does not set the timezone of the Date to UTC,
             // but instead interprets the millis as set in UTC, which is what the DatePicker provides.
             .atZone(ZoneOffset.UTC)
             .toLocalDate()
     )
 
-    val utcMillis: Long
-        get() = this.localDate
-            .atStartOfDay(ZoneOffset.UTC)
-            .toInstant()
-            .toEpochMilli()
-
     override fun toString(): String {
         val now = LocalDate.now()
 
         return when (localDate) {
             // I know this arm is unreachable, but wanted to include it for completeness.
-            // Maybe we'll want to use it for something else later.Expand commentComment on line R209
+            // Maybe we'll want to use it for something else later.
             now.plusDays(1) -> "Tomorrow"
             now -> "Today"
             now.minusDays(1) -> "Yesterday"
@@ -259,152 +253,3 @@ class Date private constructor(private val localDate: LocalDate): ChronoLocalDat
         }
     }
 }
-
-// TODO: Actual implementation in Rust
-fun graphemeStringLength(string: String): Int = string.length
-// TODO: Actual implementation in Rust
-fun graphemeStringTake(string: String, count: Int): String = string.take(count)
-
-//class Grapheme(private val inner: CharSequence) {
-//    override fun toString(): String = this.inner.toString()
-//    override fun equals(other: Any?): Boolean = this.inner == other
-//    override fun hashCode(): Int = this.inner.hashCode()
-//}
-//
-//// TODO: move this to Rust. The backend should be in charge of processing input. This is here for now to satisfy ui tests.
-///** A wrapper around [String] that operates on [**graphemes**](https://en.wikipedia.org/wiki/Grapheme) instead of [characters][Char] or *code points*.
-// *
-// * This is useful to manage a String where you only care about *visual* character units. */
-//class GraphemeString(private val inner: String): Comparable<String>, Iterable<Grapheme> {
-//    constructor() : this(String())
-//
-//    val length: Int = run {
-//        // Why forEach does not catch NoSuchElementException?? That's beyond me...
-//        @Suppress("SpellCheckingInspection")
-//        val iter = this@GraphemeString.charIdxIterator()
-//        var count = 0
-//        while (iter.next() != BreakIterator.DONE) {
-//            count ++
-//        }
-//        count
-//    }
-//
-//    /** Get the **grapheme** at the **index**.
-//     *
-//     * This will not work the same as [String]'s *get()*,
-//     * as that indexes over [Char]s, but this indexes over **graphemes**.
-//     *
-//     * @throws IndexOutOfBoundsException */
-//    operator fun get(index: Int): Grapheme {
-//        // num of chars >= num of graphemes (always)
-//        if (index > this.inner.length) {
-//            throw IndexOutOfBoundsException("Provided index $index, but the String only has ${this.inner.length} characters.")
-//        }
-//
-//        var count = 0
-//        this.iterator().forEach { grapheme ->
-//            if (count == index) {
-//                return grapheme
-//            }
-//            count++
-//        }
-//
-//        throw IndexOutOfBoundsException("Provided index $index, but the String only has $count graphemes.")
-//    }
-//
-//    /** Returns a [GraphemeString] containing **graphemes** of the original string at the specified range of **indices**.
-//     *
-//     * @throws IndexOutOfBoundsException
-//     * @throws IllegalArgumentException if [startIndex] > [endIndex] */
-//    fun subSequence(startIndex: Int, endIndex: Int): GraphemeString {
-//        if (startIndex > endIndex) {
-//            throw IllegalArgumentException("startIndex ($startIndex) must not be greater than endIndex ($endIndex).")
-//        }
-//        // num of chars >= num of graphemes (always)
-//        if (startIndex >= this.inner.length) {
-//            throw IndexOutOfBoundsException("Provided startIndex $startIndex, but the String only has ${this.inner.length} characters.")
-//        }
-//        if (endIndex >= this.inner.length) {
-//            throw IndexOutOfBoundsException("Provided endIndex $endIndex, but the String only has ${this.inner.length} characters.")
-//        }
-//
-//        val it = this.charIdxIterator()
-//        var startCharIndex: Int? = null
-//        var endCharIndex: Int? = null
-//        var count = 0
-//        @Suppress("VariableInitializerIsRedundant")
-//        var prev = 0
-//        var current = 0
-//
-//        while (true) {
-//            prev = current
-//            current = it.next()
-//            if (current == BreakIterator.DONE) {
-//                break
-//            }
-//
-//            if (count == startIndex) {
-//                startCharIndex = prev
-//            }
-//            if (count == endIndex) {
-//                endCharIndex = current
-//            }
-//
-//            if (startCharIndex != null && endCharIndex != null) {
-//                return GraphemeString(this.inner.slice(startCharIndex..endCharIndex))
-//            }
-//
-//            count++
-//        }
-//
-//        if (startCharIndex == null) {
-//            throw IndexOutOfBoundsException("Provided startIndex $startIndex, but the String only has $count graphemes.")
-//        }
-//        @Suppress("KotlinConstantConditions")
-//        if (endCharIndex == null) {
-//            throw IndexOutOfBoundsException("Provided endIndex $endIndex, but the String only has $count graphemes.")
-//        }
-//
-//        throw InternalError("Neither startCharIndex or endCharIndex were null, but the function did not return the string slice.")
-//    }
-//
-//    fun take(count: Int): GraphemeString {
-//        return if (this.length <= count) {
-//            this
-//        } else {
-//            this.subSequence(0, count)
-//        }
-//    }
-//
-//    private fun charIdxIterator(): BreakIterator {
-//        val it = BreakIterator.getCharacterInstance()
-//        it.setText(this@GraphemeString.inner)
-//        return it
-//    }
-//
-//    override fun iterator(): Iterator<Grapheme> = object: Iterator<Grapheme> {
-//        @Suppress("SpellCheckingInspection")
-//        val iter = this@GraphemeString.charIdxIterator()
-//        var prevBoundary = 0
-//
-//        override fun next(): Grapheme {
-//            this.prevBoundary = this.iter.current()
-//            val currentBoundary = this.iter.next()
-//
-//            if (currentBoundary == BreakIterator.DONE) {
-//                throw NoSuchElementException()
-//            }
-//
-//            return Grapheme(this@GraphemeString.inner.subSequence(this.prevBoundary, currentBoundary))
-//        }
-//
-//        override fun hasNext(): Boolean = (this.iter.clone() as BreakIterator).next() == BreakIterator.DONE
-//    }
-//
-//    override fun compareTo(other: String): Int = this.inner.compareTo(other)
-//
-//    override fun toString(): String = this.inner
-//
-//    override fun equals(other: Any?): Boolean = this.inner == other
-//    override fun hashCode(): Int = this.inner.hashCode()
-//}
