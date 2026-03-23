fb822761cc7307e608640e22a8c1695cd1b34916
Megadash452 <megadash452@proton.me>
2026-03-23 00:50:06 -0400

    Refactored: moved `*Work()` to Dispatch.kt, moved `RecentCurrencies` to `RecentItems` and made it generic.

---
+++ android/app/src/main/java/com/example/budgiet/RecentItems.kt
@@ -0,0 +1,148 @@
+package com.example.budgiet
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.platform.LocalContext
+import java.io.File
+import java.util.Currency as JCurrency
+import androidx.compose.ui.graphics.Color as CColor
+
+sealed class RecentItems<T> {
+    object Currency: RecentItems<JCurrency>() {
+        override fun fromString(s: String): JCurrency
+                = JCurrency.getInstance(s)
+
+        override fun toString(item: JCurrency): String
+                = item.currencyCode
+    }
+    object Color: RecentItems<CColor>() {
+        override fun fromString(s: String): CColor
+                = CColor(s.toULong())
+
+        override fun toString(item: CColor): String
+                = item.value.toString()
+    }
+
+    private lateinit var state: MutableState<Result<SnapshotStateList<T>>?>
+
+    /** Returns an ordered [List] of **recent items** with type `T`, sorted by *most recent use*.
+     *
+     * The return value tells the state of the data:
+     *  * **`null`**: The data is still being loaded.
+     *  * **[Result.Err]**: There was an error loading the data.
+     *  * **[Result.Ok]**: The data finished loading successfully.
+     *
+     *  Since this is a [Composable] with an internal [MutableState],
+     *  changes in the state will propagate to the caller and it will be recomposed. */
+    @Composable
+    fun items(): State<Result<List<T>>?> {
+        val context = LocalContext.current
+        this.state = rememberWork {
+            this.getFile(context)
+                // Read the entirety of the file to move around the elements.
+                .readText()
+                .split('\n')
+                // Last element will always be empty because the file always ends with newLine (unless it is empty).
+                .dropLast(1)
+                .map { s -> this.fromString(s) }
+                .toMutableStateList()
+                .also { Log.i(instanceName, "Loaded ${this.instanceName} from storage.") }
+        }
+
+        if (this.state.value is Result.Err) {
+            Log.e(instanceName, "Error reading ${this.instanceName} from storage: ${(this.state.value as Result.Err).error}")
+        }
+
+        return this.state
+    }
+
+    /** Removes (clears) all items from the *ordered list* in memory and from the file in storage. */
+    fun clear(context: Context) {
+        if (!this::state.isInitialized) {
+            Log.w(instanceName, this.uninitStateMsg)
+            return
+        }
+
+        // Clear in memory
+        this.state.value = Result.Ok(mutableStateListOf())
+        // Clear in storage
+        dispatchWork {
+            this.getFile(context)
+                .writeText("")
+            Log.i(instanceName, "Cleared ${this.instanceName} in storage.")
+        }
+    }
+
+    /** Marks an **item** as recently used (i.e. it was just selected),
+     * moving it to the front of the [List] of recent items,
+     * which is **sorted** by latest use.
+     *
+     * This function will also write to the [File] in storage the same content as the [List] in memory.
+     *
+     * See [RecentItems.items] to read from this [List]. */
+    fun moveToFront(item: T, context: Context) {
+        if (!this::state.isInitialized) {
+            Log.w(this.instanceName, this.uninitStateMsg)
+            return
+        }
+
+        when (this.state.value) {
+            null, is Result.Err -> this.state.value = Result.Ok(mutableStateListOf())
+            is Result.Ok -> { }
+        }
+        val orderedItems = this.state.value!!.unwrap() as MutableList<T>
+
+        val itemStr = this.toString(item)
+        Log.i(this.instanceName, "Moving item \"$itemStr\" to the front of MutableStateList in memory.")
+
+        // Apply to mutable list in memory
+        // Find currency in the argument
+        when (val idx = orderedItems.indexOf(item)) {
+            // The currency was already first in the list; do nothing.
+            0 -> { }
+            // Currency was not found in the List, so it must be prepended.
+            -1 -> orderedItems.add(0, item)
+            // Remove target currency (arg) from the List, and put it in the front.
+            else -> {
+                orderedItems.add(0, orderedItems.removeAt(idx))
+            }
+        }
+
+        // Apply to storage
+        dispatchWork {
+            Log.i(this.instanceName, "Moving item \"$itemStr\" to the front of File in storage.")
+            this.getFile(context)
+                // Write the modified list
+                .writeText(orderedItems.joinToString(
+                    separator = "",
+                    truncated = "",
+                    transform = { item -> "${this.toString(item)}\n" },
+                ))
+        }
+    }
+
+    private val instanceName = "RecentItems.${this::class.simpleName!!}"
+    private val uninitStateMsg = "${this.instanceName}.state must be initialized before calling clear() or moveToFront()."
+
+    /** Whether this [getFile] has yet to be called. */
+    private var fileFirstAccess = true
+    /** Returns the file path that contains this instance's data in storage.
+     *
+     * Creates the file if necessary. */
+    private fun getFile(context: Context): File
+        = File(context.filesDir, "RecentItems")
+            .also { if (this.fileFirstAccess) it.mkdirs() }
+            .resolve("${this::class.simpleName!!}.txt")
+            .also { if (this.fileFirstAccess) it.createNewFile() }
+            .also { this.fileFirstAccess = false }
+
+    // protected abstract val itemClass: Class<T>
+    protected abstract fun fromString(s: String): T
+    protected abstract fun toString(item: T): String
+}
--- android/app/src/main/java/com/example/budgiet/Utils.kt
+++ android/app/src/main/java/com/example/budgiet/Utils.kt
@@ -1,48 +1,19 @@
 package com.example.budgiet
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.Currency
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 
 @SuppressLint("ExperimentalAnnotationRetention")
 @RequiresOptIn(message = "This part of the API is visible only for testing.")
 internal annotation class UsableInTestsOnly
-
-
-/** An [Executor][java.util.concurrent.Executor] containing the *single thread* that will run *blocking tasks*. */
-private val WORKER_THREAD = Executors.newSingleThreadScheduledExecutor()
-/** The **ID** of the [Thread] in the *single-threaded executor* [WORKER_THREAD].
- *
- * After it is first initialized, the **ID** will not change,
- * because the code it runs will never *throw* an [Exception],
- * so the thread will not terminate until the end of the program.
- *
- * The value does not need to be put in a [Mutex][kotlinx.coroutines.sync.Mutex],
- * as only the worker thread can modify this value. */
-private var WORKER_THREAD_ID: Long? = null
-private fun isWorkerThread(): Boolean = WORKER_THREAD_ID != null && Thread.currentThread().id == WORKER_THREAD_ID
-private fun setWorkerThreadId(executor: Executor) {
-    if (executor == WORKER_THREAD && WORKER_THREAD_ID == null) {
-        WORKER_THREAD_ID = Thread.currentThread().id
-    }
-}
 
 /** A *sum type* that represents an operation's **result**.
  *
@@ -111,110 +82,6 @@ sealed class Result<out T> {
         return when(this) {
             is Ok -> Ok(transform(this.value))
             is Err -> this
-        }
-    }
-}
-
-/** Run a **task** in a *single-threaded* work [Executor],
- * and [remember] the value in a [Composable].
- *
- * This function adds the **task** to the executor and immediately returns a `mutableStateOf(null)`.
- * While the task waits to be executed (and while it is being executed),
- * the *UI* thread can continue the rendering process without having to wait for work to be done.
- *
- * After the **task** is finished, the returned [MutableState] is updated to contain a [Result]:
- * either the *success* value produced by the **task** Callback,
- * or an *error value* if the **task** threw an [Exception] ([Throwable]).
- * Throwing an [Exception] in a [Composable] is not ideal since it will crash the program if not caught,
- * so this function will automatically catch [Exception]s and put it in the [Result] instead.
- *
- * Optionally, the caller can pass a custom [Executor] to run the work in instead of the default **worker thread**. */
-@Composable
-fun <T> rememberWork(
-    executor: Executor = WORKER_THREAD,
-    task: suspend () -> T
-): MutableState<Result<T>?> {
-    val state = remember { mutableStateOf<Result<T>?>(null) }
-    suspend fun runTask()
-        // Don't allow an exception to terminate the worker thread; gotta catch em all.
-        = try {
-            Result.Ok(task())
-        } catch (e: Throwable) {
-            Result.Err(e)
-        }
-
-    LaunchedEffect(Unit) {
-        withContext(executor.asCoroutineDispatcher()) {
-            setWorkerThreadId(executor)
-
-            state.value = runTask()
-        }
-    }
-
-    return state
-}
-
-/** Run a **task** in a *single-threaded* work [Executor],
- * returning the value that the **task** produced.
- *
- * This function adds the **task** to the Executor and *suspends* while waiting for the **task** to produce a result.
- * Like [rememberWork], this function *not rethrow* any [Exception]s thrown by the **task**.
- * Instead, a [Result] is returned.
- *
- * Optionally, the caller can pass a custom [Executor] to run the work in instead of the default **worker thread**.
- *
- * > Note: If this function detects that it is being called from the *default* **worker thread**,
- * > it will just run the *task* in the same thread without first pushing it to the Executor and waiting its turn.
- * > This optimizes the order of running *tasks* in case the caller calls [runWork] without knowing it is in the worker thread,
- * > Although this should be extremely rare. */
-suspend fun <T> runWork(executor: Executor = WORKER_THREAD, task: suspend () -> T): Result<T> {
-    suspend fun runTask()
-        // Don't allow an exception to terminate the worker thread; gotta catch em all.
-        = try {
-            Result.Ok(task())
-        } catch (e: Throwable) {
-            Result.Err(e)
-        }
-
-    return if (isWorkerThread()) {
-        runTask()
-    } else {
-        withContext(executor.asCoroutineDispatcher()) {
-            setWorkerThreadId(executor)
-
-            runTask()
-        }
-    }
-}
-
-/** Run a **task** in a *single-threaded* work [Executor].
- * Use this if you don't want to *wait* for the value returned when the task is finished.
- *
- * Like [rememberWork], this function *not rethrow* any [Exception]s thrown by the **task**.
- * Instead, it will be printed to log.
- *
- * Optionally, the caller can pass a custom [Executor] to run the work in instead of the default **worker thread**.
- *
- * > Note: If this function detects that it is being called from the *default* **worker thread**,
- * > it will just run the *task* in the same thread without first pushing it to the Executor and waiting its turn.
- * > This optimizes the order of running *tasks* in case the caller calls [dispatchWork] without knowing it is in the worker thread,
- * > Although this should be extremely rare. */
-fun dispatchWork(executor: Executor = WORKER_THREAD, task: suspend () -> Unit) {
-    fun runTask() = runBlocking {
-        // Don't allow an exception to terminate the worker thread; gotta catch em all.
-        try {
-            task()
-        } catch (e: Throwable) {
-            Log.e("dispatchWork", e.toString())
-        }
-    }
-
-    if (isWorkerThread()) {
-        runTask()
-    } else {
-        executor.execute {
-            setWorkerThreadId(executor)
-            runTask()
         }
     }
 }
