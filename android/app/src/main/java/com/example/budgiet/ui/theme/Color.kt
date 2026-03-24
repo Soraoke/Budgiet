ce38af08c18355aada8a735352255151b95ecfc9
Megadash452 <megadash452@proton.me>
2026-03-23 22:07:55 -0400

    Fully implemented color property of TagCreatorDialog (still missing color picker ring)

--- android/app/src/main/java/com/example/budgiet/ui/theme/Color.kt
+++ android/app/src/main/java/com/example/budgiet/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ColorPalette = listOf(
+    // TODO: get a better color palette
+    Color.Cyan, Color.Magenta, Color.Yellow
+)
--- android/app/src/main/java/com/example/budgiet/ui/theme/Theme.kt
+++ android/app/src/main/java/com/example/budgiet/ui/theme/Theme.kt
@@ -1,8 +1,8 @@
 package com.example.budgiet.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -11,47 +11,65 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private var darkColorScheme: ColorScheme? = null
+val DarkColorScheme: ColorScheme
+    @Composable get() {
+        if (darkColorScheme != null) {
+            return darkColorScheme!!
+        }
+        darkColorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Dynamic color is available on Android 12+
+            dynamicDarkColorScheme(LocalContext.current)
+        } else {
+            darkColorScheme(
+                primary = Purple80,
+                secondary = PurpleGrey80,
+                tertiary = Pink80
+            )
+        }
+        return darkColorScheme!!
+    }
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private var lightColorScheme: ColorScheme? = null
+val LightColorScheme: ColorScheme
+    @Composable get() {
+        if (lightColorScheme != null) {
+            return lightColorScheme!!
+        }
+        lightColorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Dynamic color is available on Android 12+
+            dynamicLightColorScheme(LocalContext.current)
+        } else {
+            lightColorScheme(
+                primary = Purple40,
+                secondary = PurpleGrey40,
+                tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+                /* Other default colors to override
+                background = Color(0xFFFFFBFE),
+                surface = Color(0xFFFFFBFE),
+                onPrimary = Color.White,
+                onSecondary = Color.White,
+                onTertiary = Color.White,
+                onBackground = Color(0xFF1C1B1F),
+                onSurface = Color(0xFF1C1B1F),
+                */
+            )
+        }
+        return lightColorScheme!!
+    }
 
 @Composable
 fun BudgietTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = if (darkTheme) {
+            DarkColorScheme
+        } else {
+            LightColorScheme
+        },
         typography = Typography,
         content = content
     )
