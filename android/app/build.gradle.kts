2be4a3d6007a11c4eb12579b7f89a11c495777b0
Megadash452 <megadash452@proton.me>
2026-06-01 17:46:22 -0400

    Added 'Total' column to items list for each Item.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -119,6 +119,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.paging.common)
     implementation(libs.androidx.lifecycle.viewmodel)
     testImplementation(libs.junit)
